f05a0f9fed5bd595f009111a6ec5d62e4f0f935f
Tyler Phung <tylerjphung@gmail.com>
2020-05-24 01:50:32 -0700

    week 8

--- GwajaFight/Assets/CountdownTimer.cs
+++ GwajaFight/Assets/CountdownTimer.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class CountdownTimer : MonoBehaviour
 {
     float currentTime = 0f;
-    float startingTime = 10f;
+    float startingTime = 120f;
 
     public Text countdownText;
 
