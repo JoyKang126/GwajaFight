69d881f3366729fcd55af636f8b5e13d6a20a3a8
felixsu2006 <56893129+felixsu2006@users.noreply.github.com>
2020-05-29 14:15:09 -0700

    push/eat update
    updated push and eat

--- GwajaFight/Assets/Push.cs
+++ GwajaFight/Assets/Push.cs
@@ -4,29 +4,31 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
-
+    
     //public float knockback;
+    public float knockTime;
+
     private float timeBtwAttack;
     public float startTimeBtwAttack;
 
     public Transform attackPos;
-    public float attackRange;
     public LayerMask whatIsEnemies;
     public KeyCode push;
 
     public float attackRangeX;
     public float attackRangeY;
-    public float knockTime;
+    
 
+    
     void Update()
     {
         if(timeBtwAttack <= 0)
         {
-            if (!this.GetComponent<PlayerMovement>().getHold() && Input.GetKey(push))
+            if (!GetComponent<PlayerMovement>().getHold() && Input.GetKey(push))
             {
                 print("asdfasdf");
-                this.GetComponent<PlayerMovement>().setFreeze(true);
-                StartCoroutine(freezeTime(0.5f));
+                GetComponent<PlayerMovement>().setFreeze(true);
+                StartCoroutine(freezeTime(0.1f));
                 Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY),0, whatIsEnemies);
                 for (int i = 0; i < enemiesToDamage.Length; i++)
                 {
@@ -52,6 +54,12 @@ public class Push : MonoBehaviour
         }
     }
 
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(attackPos.position, new Vector3(attackRangeX, attackRangeY));
+    }
+
     private IEnumerator KnockCo(Rigidbody2D enemy, PlayerMovement other)
     {
         if (enemy != null)
@@ -66,9 +74,49 @@ public class Push : MonoBehaviour
     private IEnumerator freezeTime(float f)
     {
         yield return new WaitForSeconds(f);
-        this.GetComponent<PlayerMovement>().setFreeze(false);
+        GetComponent<PlayerMovement>().setFreeze(false);
     }
 
+    /*
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("ASD");
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            PlayerMovement temp = other.GetComponent<PlayerMovement>();
+            if (enemy != null)
+            {
+                temp.setFreeze(true);
+                temp.setPush(true);
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * knockback;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                //print(difference);
+                StartCoroutine(KnockCo(enemy, temp));
+            }
+
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy, PlayerMovement other)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+        }
+        other.setFreeze(false);
+        other.setPush(false);
+    }
+
+    private IEnumerator freezeTime(float f)
+    {
+        yield return new WaitForSeconds(f);
+        GetComponent<PlayerMovement>().setFreeze(false);
+    }
+
+    /*
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
@@ -97,6 +145,6 @@ public class Push : MonoBehaviour
 
     */
 
-    
-    
+
+
 }
