2c6fafebe82b313ad7e3d38a817c25e9e4cb6df6
Nidhi <nidhipanchal1119@gmail.com>
2020-07-01 17:57:03 -0700

    Ending screens

--- GwajaFight/Assets/GameManager.cs
+++ GwajaFight/Assets/GameManager.cs
@@ -6,12 +6,61 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    GameObject[] p1Win;
+    GameObject[] p2Win;
+    GameObject[] noWin;
 
+    public void Start()
+    {
+        p1Win = GameObject.FindGameObjectsWithTag("P1Wins");
+        foreach (GameObject g in p1Win)
+        {
+            Debug.Log("p1");
+            g.SetActive(false);
+        }
+        p2Win = GameObject.FindGameObjectsWithTag("P2Wins");
+        foreach (GameObject g in p2Win)
+        {
+            g.SetActive(false);
+        }
+        noWin = GameObject.FindGameObjectsWithTag("NoWinner");
+        foreach (GameObject g in noWin)
+        {
+            g.SetActive(false);
+        }
+    }
     public void EndGame()
     {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            GameObject player1 = players[0];
+            GameObject player2 = players[1];
+            int p1Pts = player1.GetComponent<PointsScript>().getScore();
+            int p2Pts = player2.GetComponent<PointsScript>().getScore();
+            FindObjectOfType<GameManager>().EndGame();
+            if (p1Pts > p2Pts)
+            {
+                Debug.Log("p1 wins");
+                foreach (GameObject g in p1Win)
+                {
+                    g.SetActive(true);
+                }
+            } else if (p2Pts > p1Pts) {
+                Debug.Log("p2 wins");
+                foreach (GameObject g in p2Win)
+                {
+                    g.SetActive(true);
+                }
+            } else {
+                Debug.Log("no win");
+                foreach (GameObject g in noWin)
+                {
+                    Debug.Log("obj 1");
+                    g.SetActive(true);
+                }
+            }
             Debug.Log("GAME OVER");
         }
     }
